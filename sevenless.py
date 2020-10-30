e9107ec56c82e6f9fc1876ddf4d9ae173545241e
Ksalgado8 <72320414+Ksalgado8@users.noreply.github.com>
2020-10-29 19:53:23 -0700

    Update sevenless.py

--- sevenless.py
+++ sevenless.py
@@ -4,3 +4,6 @@ start = 0
 end   = 99
 divisor=7
 print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+for hw2a in range(0, 99):
+...     if hw2a % 7 != 0:
+...         print(hw2a)
